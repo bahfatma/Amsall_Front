c67d7f0625a0f184f0f621f67fc445229cfe4b68
bahfatma <bah06fatoumata@gmail.com>
2025-01-15 22:25:44 +0100

    ajout des verifications du password

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -15,17 +15,51 @@ export class RegisterComponent {
     phoneNumber: ''
   };
 
+  passwordErrors: string[] = []; 
+  isSubmitted: boolean = false;
+
   constructor(private userService: UserService) {}
 
+  validatePassword(password: string): string[] {
+    const errors: string[] = [];
+
+    if (password.length < 8) {
+      errors.push('Le mot de passe doit contenir au moins 8 caractères.');
+    }
+
+    if (!/[A-Z]/.test(password)) {
+      errors.push('Le mot de passe doit contenir au moins une lettre majuscule.');
+    }
+
+    if (!/[a-z]/.test(password)) {
+      errors.push('Le mot de passe doit contenir au moins une lettre minuscule.');
+    }
+
+    if (!/\d/.test(password)) {
+      errors.push('Le mot de passe doit contenir au moins un chiffre.');
+    }
+
+    if (!/[@$!%*?&]/.test(password)) {
+      errors.push('Le mot de passe doit contenir au moins un caractère spécial.');
+    }
+
+    return errors;
+  }
+
   onSubmit() {
-    this.userService.addUser(this.user).subscribe(
-      response => {
-        alert('Utilisateur créé avec succès!');
-      },
-      error => {
-        console.error(error);
-        alert('Erreur lors de la création de l\'utilisateur');
-      }
-    );
+    this.isSubmitted = true; // Marque le formulaire comme soumis
+    this.passwordErrors = this.validatePassword(this.user.passWord); 
+
+    if (this.passwordErrors.length === 0) {
+      this.userService.addUser(this.user).subscribe(
+        response => {
+          alert('Utilisateur créé avec succès!');
+        },
+        error => {
+          console.error(error);
+          alert('Erreur lors de la création de l\'utilisateur.');
+        }
+      );
+    }
   }
 }
